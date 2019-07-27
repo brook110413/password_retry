79d9be8976f6e6c1d5517cf65c79edfbd3cae828
Jason Hung <brook110413@gmail.com>
2019-07-27 18:06:29 +0800

    update

--- password.py
+++ password.py
@@ -1,15 +1,14 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
+	i = i - 1
 	password_1 = input('請輸入您的密碼: ')
 	if password != password_1:
-		i = i - 1
-		print('密碼錯誤 還有', i, '次機會 ')
-		if i == 0:
-			break
+		print('密碼錯誤')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('嘗試3次錯誤 帳號已被鎖')
 	else:
 		print('登入成功')
 		break
-
-
-
